d4fb99b17cf9f233b1c6f6a652ebc39e73f29d6f
Magaly_Y <a3521110208@alumno.uttehuacan.edu.mx>
2024-12-03 22:50:39 -0600

    Octava actualización mejorada 5.0- Sueños de papel

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
 const CACHE_NAME = 'v1';
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/chat.html',
+    '/perfil.html',
+    '/libros.html',
+    'styles.css',
+    'assets/logo.png',
+    'assets/icons/icon-192x192.png',
+    'assets/icons/icon-512x512.png',
+    'firebaseConfig.js',
+    'scripts.js'
+];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/chat.html',
-                '/perfil.html',
-                '/libros.html',  
-                'styles.css',
-                'assets/logo.png',
-                'assets/icons/icon-192x192.png',
-                'assets/icons/icon-512x512.png',
-                'firebaseConfig.js',
-                'scripts.js',
-                '/manifest.json'
-            ]);
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
@@ -35,16 +36,16 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes('/chats')) {
-        return;  // No cachear peticiones al chat en tiempo real
-    }
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request).then((networkResponse) => {
-                return caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, networkResponse.clone());
-                    return networkResponse;
-                });
+            return response || fetch(event.request);
+        }).catch(() => {
+            return caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                } else if (event.request.mode === 'navigate') {
+                    return caches.match('/index.html');
+                }
             });
         })
     );
